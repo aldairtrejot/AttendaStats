a88942306f2dda722964d31357c95ecafca6ed0d
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-09-09 09:48:37 -0600

    more chnegs

--- login.php
+++ login.php
@@ -45,8 +45,15 @@
   <!-- End Navbar -->
 
   <main class="main-content mt-0">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+    
+<!--
+<div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
       style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+
+-->
+<div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+      style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
